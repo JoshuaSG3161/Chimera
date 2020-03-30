6b8291a3277eae1736473fb1c06cc742fee86a47
JoshuaSG3161 <32961794+JoshuaSG3161@users.noreply.github.com>
2020-03-30 18:40:11 -0400

    Update Ye.py

--- Ye.py
+++ Ye.py
@@ -61,76 +61,150 @@ class LocationHeading:
 #This function will decide which way to turn
 class turnBoi:
 
-    def __init__(self):
+    def __init__(self, xCoordinate, yCoordinate, slowDown):
         self.leftCounter = 0
-        self.rightCounter = 0        
-    
-#Calc the number of beams touching an object on the left
-    def distanceCalcLeft(self):
-        for l in range (0,8):
-            if laser.laserRanges[l] < 4:
-                self.leftCounter = self.leftCounter + 1
-        return self.leftCounter
-    
-##Calc the number of beams touching an object on the right
-    def distanceCalcRight(self):
-        for r in range (8,15):
-            if laser.laserRanges[r] < 4:
-                self.rightCounter = self.rightCounter + 1
-        return self.rightCounter
-    
+        self.rightCounter = 0
+        self.goFirst = 0
+        self.pointBX = xCoordinate
+        self.pointBY = yCoordinate
+        self.slowSpeed = self.pointBY - slowDown
+
+#This function looks at which beamns of the LiDAR are coming in contact with objects
+    def scan(self):
+        new_range = list(laser.laserRanges)
+        new_ranges = new_range[:]
+        min_range = new_ranges[0]
+        index = 0
+        while True:
+            for i in range(len(new_ranges)):
+                if min_range>new_ranges[i]:
+                    min_range = new_ranges[i]
+                    index = i
+                    
+            if index == 0 and laser.laserRanges[0] <= 1.3:
+                collision = True
+                self.leftCounter = 1
+                break
+            elif index == 15 and laser.laserRanges[15] <= 1.3:
+                collision = True
+                self.rightCounter = 1
+                break
+            elif index == 1 and laser.laserRanges[1] <= 1.5:
+                collision = True
+                self.leftCounter = 1
+                break
+            elif index == 14 and laser.laserRanges[14] <= 1.5:
+                collision = True
+                self.rightCounter = 1
+                break
+            elif index == 2 and laser.laserRanges[2] <= 1.7:
+                collision = True
+                self.leftCounter = 1
+                break
+            elif index == 13 and laser.laserRanges[13] <= 1.7:
+                collision = True
+                self.rightCounter = 1
+                break
+            elif index == 3 and laser.laserRanges[3] <= 2.1:
+                collision = True
+                self.leftCounter = 1
+                break
+            elif index == 12 and laser.laserRanges[12] <= 2.1:
+                collision = True
+                self.rightCounter = 1
+                break
+            elif index == 4 and laser.laserRanges[4] <= 2.6:
+                collision = True
+                self.leftCounter = 1
+                self.goFirst =1
+                break
+            elif index == 11 and laser.laserRanges[11] <= 2.6:
+                collision = True
+                self.rightCounter = 1
+                self.goFirst =1
+                break
+            elif index == 5 and laser.laserRanges[5] <= 3.6:
+                collision = True
+                self.leftCounter = 1
+                self.goFirst =1
+                break
+            elif index == 10 and laser.laserRanges[10] <= 3.6:
+                collision = True
+                self.rightCounter = 1
+                self.goFirst = 1
+                break
+            elif index == 6 and laser.laserRanges[6] <= 5.9:
+                collision = True
+                self.leftCounter = 1
+                self.goFirst = 1
+                break
+            elif index == 9 and laser.laserRanges[9] <= 5.9:
+                collision = True
+                self.rightCounter = 1
+                self.goFirst = 1
+                break
+            else:
+                collision = False
+            del new_ranges[index:index+1]
+        return collision
+
+#This function decides which way to turn
     def turnNow(self):
-        minRange = 50 #initialize minRange to a value larger than what will be recieved
-        for x in range(0, 15): #iterate through the ranges list
-            if laser.laserRanges[x] < minRange: #if the current range is smaller than the smallest know range
-                minRange = laser.laserRanges[x] #update the range
-        if minRange < 3: #if there is something closer than 3m infront of the rover
-            print("Turningggggggggggggggggggg")
-            if (self.rightCounter>self.leftCounter):
+        print("Turning")
+        if (self.goFirst > self.rightCounter) or (self.goFirst > self.leftCounter):
+            wheel.drive_wheels(0.5, 0.5)
+            print("I can fit dawg")
+        else:
+            if self.rightCounter < self.leftCounter:
                 wheel.drive_wheels(1, -1)
                 self.rightCounter = 0
                 self.leftCounter = 0
-                #SKRRRT=(LeftTurn())
                 print("Left Turn")
-            if (self.rightCounter<self.leftCounter):
+            elif self.leftCounter < self.rightCounter:
                 wheel.drive_wheels(-1, 1)
                 self.rightCounter = 0
                 self.leftCounter = 0
-                #SKRRRT=(RightTurn())
                 print("Right Turn")
+            
+    def stoppyBoiY(self):
+        if locHead.y < self.pointBY:
+            wheel.drive_wheels(0.0, 0.0)
+            print("--Stopping all wheels now--")
+        elif locHead.y < self.slowSpeed:
+            wheel.drive_wheels(0.1, 0.1)
+            print("Slowing down now")
         else:
             wheel.drive_wheels(1, 1)
-            print("Driving Forward")   
-            
-    def stoppyBoi(self):
-        if locHead.y < -11:
-            wheel.drive_wheels(-0.5, -0.5)
-        elif locHead.y < -10 and locHead.y > -11:
-            wheel.drive_wheels(0, 0)
+    
+#This function finds the X-coordinate
+    def stoppyBoiX(self):
+        if locHead.x < self.pointBX:
+            wheel.drive_wheels(0.0, 0.0)
+            print("--Stopping all wheels now--")
+        elif locHead.x < self.slowSpeed:
+            wheel.drive_wheels(0.1, 0.1)
+            print("Slowing down now")
         else:
             wheel.drive_wheels(1, 1)
                 
 # end of localization stuff
 
-class RoverDirection:
-    
-    def path(self, x, y):
-        angle = arctan((y - locHead.y)/(x - locHead.x))
-        while angle != locHead.heading:
-            wheel.drive_wheels(-1, 1)
-        wheel.drive_wheels(1,1)
-        
 #initiallize classes to get and send data to gazebo
 locHead  = LocationHeading()
 laser = LaserListener()
 wheel = WheelController()
-SKRRRT = turnBoi()
+SKRRRT = turnBoi(0.0, -20.0, -5.0)
 #end of initialization
 
 # start of control loop snippet
 while not rospy.is_shutdown():
-    SKRRRT.distanceCalcLeft()
-    SKRRRT.distanceCalcRight()
-    #SKRRRT.turnNow()
-    SKRRRT.stoppyBoi()
+    minRange = 50 #initialize minRange to a value larger than what will be recieved
+    for x in range(0, 15): #iterate through the ranges list
+        if laser.laserRanges[x] < minRange: #if the current range is smaller than the smallest know range
+            minRange = laser.laserRanges[x] #update the range
+    if SKRRRT.scan():
+        SKRRRT.turnNow()
+    else:
+        wheel.drive_wheels(0.5, 0.5)
     print("Passed Function")
+
